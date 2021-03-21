3c4d5c20032412d211b26cb5044b8c81be3b33aa
Rafal Jakubczyk <jakubczykrafal06@gmail.com>
2021-03-21 16:35:58 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleMinesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
